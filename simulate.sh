7937d42b716666a2c3d66b54437378963976e4f3
frenchwr <william.r.french@vanderbilt.edu>
2015-11-18 10:02:04 -0600

    Adding some logic to simulate script

--- simulate.sh
+++ simulate.sh
@@ -1,3 +1,12 @@
 #!/bin/bash
 
-bin/run_md -N 125
+simtype=$1
+
+if [[ $simtype == "bigsim" ]]; 
+then
+   echo "running: bin/run_md -N 5000 -ts 10000 -xyz 1000 -o 50"
+   bin/run_md -N 5000 -ts 10000 -xyz 1000 -o 50
+else
+   echo "running: bin/run_md -N 125"
+   bin/run_md -N 125
+fi
